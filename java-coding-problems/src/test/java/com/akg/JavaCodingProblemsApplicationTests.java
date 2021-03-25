c9a53d65da43d5d9b3025897e4815a25a905d949
amitkumar12788 <amitkumar12788@gmail.com>
2021-03-26 01:24:29 +0530

    new project for coding practice

---
+++ java-coding-problems/src/test/java/com/akg/JavaCodingProblemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCodingProblemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
